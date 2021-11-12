b399be20497aa1d534af3986de45f9ff3addfb10
RTLS <carter.e.pedersen@gmail.com>
2021-11-12 07:24:29 -1000

    fix config

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,10 @@
 # move said applications out of the umbrella.
 import Config
 
+for config <- "../apps/*/config/config.exs" |> Path.expand(__DIR__) |> Path.wildcard() do
+  import_config config
+end
+
 # Sample configuration:
 #
 #     config :logger, :console,
